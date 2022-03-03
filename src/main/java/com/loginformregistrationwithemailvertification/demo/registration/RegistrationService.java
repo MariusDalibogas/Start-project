cfd634ed10384acb3d4a5cdaa1219aff7cab8e5d
Marius Dalibogas <marius.dalibogas@gmail.com>
2022-03-03 14:31:46 +0200

    Created new registration folder: Created new java class:RegistrationController,RegistrationService and RegistrationRequest. Added @RestController and @RequestMapping annotations in RegistrationController.java class

---
+++ src/main/java/com/loginformregistrationwithemailvertification/demo/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.loginformregistrationwithemailvertification.demo.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request){
+        return "Works";
+    }
+}
